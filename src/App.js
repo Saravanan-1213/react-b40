f29d2694e10d60349a0e563d6e3ff2a543f92825
“SaraVanan <saravanansekar033@gmail.com>
2022-11-18 14:18:25 +0530

    Users component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+//App components declare logic + view 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User name="SaraVanan🖤💛" 
+      pic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP_jBNGDAl_gCziVLX5q8vuci3Z8VmfK8Rrdtct54&s" />
+      <User name="Ravi" 
+      pic="https://images.unsplash.com/photo-1628563694622-5a76957fd09c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW5zdGFncmFtJTIwcHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"/>
+      <User name="John"
+      pic="https://wallpaperaccess.com/full/2213424.jpg"/>
     </div>
   );
 }
 
+function User({pic,name}){
+  // console.log(props, typeof props);
+  return (
+    <>
+  <img className='user-profile-pic' src={pic}/>
+  <h1 className="user-name"> 
+  Hello <span className='user-first-name'>{name}</span></h1>
+  </>
+  );
+
+}
+
+
 export default App;
+
+
